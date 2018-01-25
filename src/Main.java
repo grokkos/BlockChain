7daed06909d32d4a99a9fbfb5e973cfaae0aed07
George <gerokkos@gmail.com>
2018-01-25 23:20:29 +0200

    Update

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,18 @@
  */
 public class Main {
 
-    /**
-     * test
-     */
+
+    public static void main(String[] args) {
+        BasicBlock genesisBlock = new BasicBlock("Hi im the first block", "0");
+        System.out.println("Hash for block 1 : " + genesisBlock.hash);
+
+        BasicBlock secondBlock = new BasicBlock("Yo im the second block",genesisBlock.hash);
+        System.out.println("Hash for block 2 : " + secondBlock.hash);
+
+        BasicBlock thirdBlock = new BasicBlock("Hey im the third block",secondBlock.hash);
+        System.out.println("Hash for block 3 : " + thirdBlock.hash);
+
+    }
+
+
 }
